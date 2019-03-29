056bc965152efc14f72326ed21d8d1da8a26b849
[leonard.achanya] <[lennardachanya.la@gmail.com]>
2019-03-29 14:55:44 +0100

    added some files/pacakges needed for the user module

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class AppNavbar extends Component{
+    state= {
+        jobs:[]
+    }
+
+    render(){
+        return (
+            <div>
+                AppNavbar
+            </div>
+        )
+    }
+
+}
+
+export default AppNavbar;
